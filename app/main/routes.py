318ed82e7c354e06b9376657a219b610506aab67
Shashanksreeram13 <shashanksreeram77@gmail.com>
2025-05-15 22:56:00 +0800

    added background to index page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, session
 from app.models import Movie
 from flask_login import current_user
+from random import sample
+from app import db
 
 ## Blueprint for main routes
 main = Blueprint('main', __name__)
@@ -10,7 +12,10 @@ main = Blueprint('main', __name__)
 def home():
     if current_user.is_authenticated:
         return redirect(url_for("movies.search"))
-    return render_template("index.html")
+
+    posters = db.session.query(Movie.Poster_Link).filter(Movie.Poster_Link != None).limit(50).all()
+    poster_urls = [p[0] for p in posters if p[0]]
+    return render_template("index.html", posters=poster_urls)
 
 @main.route("/test-db")
 def test_db():
