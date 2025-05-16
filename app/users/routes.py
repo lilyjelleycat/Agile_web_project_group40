4b340a61c95570f3e9780258e8b6ed7ef44efa72
hongfei <15838328117@163.com>
2025-05-16 20:22:15 +0800

    Merge specific files from hongfei to main (list files)

--- app/users/routes.py
+++ app/users/routes.py
@@ -2,19 +2,27 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required, login_user, logout_user, current_user
 from app import db
-from app.users.forms import RegistrationForm, LoginForm, ChangePasswordForm, ResetPasswordForm
+from app.users.forms import (
+    RegistrationForm,
+    LoginForm,
+    ChangePasswordForm,
+    ResetPasswordForm,
+)
 from app.movies.forms import SearchForm
 from app.models import Member, UserRole, Review, AnalyticsShare, ReviewShare
 
 # Blueprint for user-related routes
-users = Blueprint('users', __name__)
+users = Blueprint("users", __name__)
+
 
 @users.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        flash('You are already logged in!', 'info')
+        flash("You are already logged in!", "info")
+        if current_user.has_role('admin'):
+            return redirect(url_for("admin.dashboard"))
         return redirect(url_for("movies.search"))
-    
+
     regForm = RegistrationForm()
     if regForm.validate_on_submit():
         new_member = Member(
@@ -22,41 +30,51 @@ def register():
             firstName=regForm.first_name.data,
             lastName=regForm.last_name.data,
             email=regForm.email.data,
-            hashPwd=generate_password_hash(regForm.password.data)
+            hashPwd=generate_password_hash(regForm.password.data),
         )
         db.session.add(new_member)
-        db.session.add(UserRole(username=new_member.username, role='user'))
+        db.session.add(UserRole(username=new_member.username, role="user"))
         db.session.commit()
-        flash(f'Account created for {regForm.username.data}!', 'success')
+        flash(f"Account created for {regForm.username.data}!", "success")
         return redirect(url_for("users.login"))
 
     return render_template("register.html", form=regForm)
 
+
 @users.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        flash('You are already logged in!', 'info')
+        flash("You are already logged in!", "info")
+        if current_user.has_role('admin'):
+            return redirect(url_for("admin.dashboard"))
         return redirect(url_for("movies.search"))
-    
+
     loginForm = LoginForm()
     if loginForm.validate_on_submit():
         member = Member.query.filter_by(username=loginForm.username.data).first()
         if member and check_password_hash(member.hashPwd, loginForm.password.data):
             login_user(member, remember=loginForm.remember.data)
-            flash(f'Welcome back, {member.username}!', 'info')
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for("movies.search"))
+            flash(f"Welcome back, {member.username}!", "info")
+            if member.has_role('admin'):
+                return redirect(url_for("admin.dashboard"))
+            else:
+                next_page = request.args.get("next")
+                return (
+                    redirect(next_page) if next_page else redirect(url_for("movies.search"))
+                )
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash("Login Unsuccessful. Please check username and password", "danger")
 
     return render_template("login.html", form=loginForm)
+
 
 @users.route("/logout")
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out!', 'info')
+    flash("You have been logged out!", "info")
     return redirect(url_for("main.home"))
+
 
 @users.route("/change_password", methods=["GET", "POST"])
 @login_required
@@ -65,42 +83,52 @@ def change_password():
         changeForm = ChangePasswordForm()
         if changeForm.validate_on_submit():
             member = Member.query.filter_by(username=current_user.username).first()
-            if member and check_password_hash(member.hashPwd, changeForm.current_password.data):
+            if member and check_password_hash(
+                member.hashPwd, changeForm.current_password.data
+            ):
                 member.hashPwd = generate_password_hash(changeForm.new_password.data)
                 db.session.commit()
                 logout_user()
-                flash('Your password has been updated!', 'success')
+                flash("Your password has been updated!", "success")
                 return redirect(url_for("users.login"))
             else:
-                flash('Current password is incorrect', 'danger')
+                flash("Current password is incorrect", "danger")
         return render_template("change_user_password.html", form=changeForm)
     else:
-        flash('You need to be logged in to change your password!', 'info')
+        flash("You need to be logged in to change your password!", "info")
         return redirect(url_for("users.login"))
+
 
 @users.route("/reset_password", methods=["GET", "POST"])
 def reset_password():
     resetPasswordForm = ResetPasswordForm()
     if resetPasswordForm.validate_on_submit():
-        member = Member.query.filter_by(username=resetPasswordForm.username.data).first()
-        if member and resetPasswordForm.email.data == member.email and \
-                      resetPasswordForm.first_name.data == member.firstName and \
-                      resetPasswordForm.last_name.data == member.lastName:
+        member = Member.query.filter_by(
+            username=resetPasswordForm.username.data
+        ).first()
+        if (
+            member
+            and resetPasswordForm.email.data == member.email
+            and resetPasswordForm.first_name.data == member.firstName
+            and resetPasswordForm.last_name.data == member.lastName
+        ):
             member.hashPwd = generate_password_hash(resetPasswordForm.new_password.data)
             db.session.commit()
-            flash('Your password has been reset!', 'success')
+            flash("Your password has been reset!", "success")
             return redirect(url_for("users.login"))
         else:
-            flash('Invalid username or email', 'danger')
-    
+            flash("Invalid username or email", "danger")
+
     return render_template("reset_user_password.html", form=resetPasswordForm)
+
 
 @users.route("/profile")
 @login_required
 def profile():
     user = Member.query.get(current_user.username)
     reviews = Review.query.filter_by(username=user.username).all()
-    return render_template('profile.html', user=user, reviews=reviews)
+    return render_template("profile.html", user=user, reviews=reviews)
+
 
 @users.route("/profile/toggle_public", methods=["POST"])
 @login_required
@@ -110,6 +138,7 @@ def toggle_public():
     db.session.commit()
     return jsonify({"status": "success"})
 
+
 @users.route("/search_user")
 @login_required
 def search_user():
@@ -118,7 +147,8 @@ def search_user():
     usernames = [u.username for u in users if u.username != current_user.username]
     return jsonify(usernames)
 
-@users.route("/save_shares", methods=['POST'])
+
+@users.route("/save_shares", methods=["POST"])
 @login_required
 def save_shares():
     data = request.get_json()
@@ -132,18 +162,29 @@ def save_shares():
         if not user:
             continue
         if entry.get("review"):
-            db.session.add(ReviewShare(owner_username=current_user.username, viewer_username=user))
+            db.session.add(
+                ReviewShare(owner_username=current_user.username, viewer_username=user)
+            )
         if entry.get("analytics"):
-            db.session.add(AnalyticsShare(owner_username=current_user.username, viewer_username=user))
+            db.session.add(
+                AnalyticsShare(
+                    owner_username=current_user.username, viewer_username=user
+                )
+            )
 
     db.session.commit()
-    return jsonify({'message': 'Success'}), 200
+    return jsonify({"message": "Success"}), 200
+
 
 @users.route("/share", methods=["GET"])
 @login_required
 def share():
-    review_shares = ReviewShare.query.filter_by(owner_username=current_user.username).all()
-    analytics_shares = AnalyticsShare.query.filter_by(owner_username=current_user.username).all()
+    review_shares = ReviewShare.query.filter_by(
+        owner_username=current_user.username
+    ).all()
+    analytics_shares = AnalyticsShare.query.filter_by(
+        owner_username=current_user.username
+    ).all()
 
     shared_status = {}
     for r in review_shares:
